8da6c63f5932fe7db11d77f440496e11488ce73c
sergiuss.mic <sergiuss.mic@hotmail.com>
2017-09-11 21:39:09 +0600

    w123
    123

---
+++ SDGame/funcs.cpp
@@ -0,0 +1,364 @@
+#include <iostream>
+#include <Windows.h>
+#include <conio.h>
+#include <cstdlib>
+#include "main.h"
+
+using namespace ::std;
+
+ int z, v ;
+
+ void Blinking(int off)
+ {
+	 void* handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	 CONSOLE_CURSOR_INFO structCursorInfo;
+	 GetConsoleCursorInfo(handle, &structCursorInfo);
+	 structCursorInfo.bVisible = FALSE;
+	 SetConsoleCursorInfo(handle, &structCursorInfo);
+ }
+ void gotoxy(int xpos, int ypos)
+ {
+	 COORD scrn;
+
+	 HANDLE hOuput = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	 scrn.X = xpos - 1; scrn.Y = ypos - 1;
+
+	 SetConsoleCursorPosition(hOuput, scrn);
+ }
+ void podgotovka() {
+	 system("mode con cols=101 lines=30");
+	 setlocale(LC_ALL, "Russian");
+	 Blinking();
+	 v = 1;
+ }
+
+ void LoadSave()
+ {
+	 switch (v) {
+	 case 1: {
+
+	 }
+	 case 2: {
+
+	 }
+	 case 3: {
+
+	 }
+	}
+ }
+
+
+void Start() {
+
+}
+void Load() {
+	v = 1;
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+	system("cls");
+	gotoxy(44,13);
+	cout << "1 Cохранение";
+	gotoxy(44,15);
+	cout << "2 Cохранение";
+	gotoxy(44,17);
+	cout << "3 Cохранение";
+	//Бесконечный цикл
+	while (z != 111) {
+
+		if (v == 1) {
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+			gotoxy(44, 15);
+			cout << "2 Cохранение";
+			gotoxy(44, 17);
+			cout << "3 Cохранение";
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+			gotoxy(44, 13);
+			cout << "1 Cохранение";
+		}
+		else
+		if (v == 2) {
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+			gotoxy(44, 13);
+			cout << "1 Cохранение";
+			gotoxy(44, 17);
+			cout << "3 Cохранение";
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+			gotoxy(44, 15);
+			cout << "2 Cохранение";
+		}
+		else
+		if (v == 3) {
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+			gotoxy(44, 13);
+			cout << "1 Cохранение";
+			gotoxy(44, 15);
+			cout << "2 Cохранение";
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+			gotoxy(44, 17);
+			cout << "3 Cохранение";
+		}
+
+		// Это обработчик нажатий
+		if (_kbhit()) {
+			switch (_getch()) {
+			case 'w': {
+				if (v == 1)
+					v = 3;
+				else
+					if (v == 2)
+						v = 1;
+					else
+						if (v == 3)
+							v = 2;
+				break;
+			}
+			case 's': {
+				if (v == 1)
+					v = 2;
+				else
+					if (v == 2)
+						v = 3;
+					else
+						if (v == 3)
+							v = 1;
+				break;
+			}
+			case 'W': {
+				if (v == 1)
+					v = 3;
+				else
+					if (v == 2)
+						v = 1;
+					else
+						if (v == 3)
+							v = 2;
+				break;
+			}
+			case 'S': {
+				if (v == 1)
+					v = 2;
+				else
+					if (v == 2)
+						v = 3;
+					else
+						if (v == 3)
+							v = 1;
+				break;
+			}
+			case 13: {
+				if (v == 1)
+					LoadSave();
+				else
+				if (v == 2)
+					LoadSave();
+				else
+				if (v == 3)
+					LoadSave();
+				break;
+			}
+			case 8: {
+
+			system("cls");
+				Menu();
+				break;
+			}
+			}
+		}
+		
+
+
+	}
+}
+void Quit() {
+	v = 1;
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+	system("cls");
+	gotoxy(36, 7);
+	cout << "Вы действительно хотите выйти?";
+	gotoxy(36, 14);
+	cout << "              ДА";
+	gotoxy(36, 16);
+	cout << "             НЕТ";
+
+	while (z != 111) {
+
+		// Стрелочки выбора
+		if (v == 1) {
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+			gotoxy(36, 16);
+			cout << "             НЕТ    ";
+
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+			gotoxy(36, 14);
+			cout << "              ДА    ";
+		}
+		if (v == 2) {
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+			gotoxy(36, 14);
+			cout << "              ДА    ";
+
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+			gotoxy(36, 16);
+			cout << "             НЕТ    ";
+		}
+
+		// Это обработчик нажатий
+		if (_kbhit()) {
+			switch (_getch()) {
+			case 'w': {
+				if (v == 1)
+					v = 2;
+				else
+					if (v == 2)
+						v = 1;
+				break;
+			}
+			case 's': {
+				if (v == 1)
+					v = 2;
+				else
+					if (v == 2)
+						v = 1;
+
+				break;
+			}
+			case 'W': {
+				if (v == 1)
+					v = 2;
+				else
+					if (v == 2)
+						v = 1;
+				break;
+			}
+			case 'S': {
+				if (v == 1)
+					v = 2;
+				else
+					if (v == 2)
+						v = 1;
+				break;
+			}
+			case 13: {
+				if (v == 1)
+					z = 111;
+				else
+					if (v == 2)
+						system("cls");
+				Menu();
+
+				break;
+			}
+			}
+		}
+
+	}
+
+}
+void Menu() {
+	v = 1;
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+	gotoxy(46, 13);
+	cout << "  СТАРТ  ";
+	gotoxy(46, 15);
+	cout << "ПРОДОЛЖИТЬ";
+	gotoxy(46, 17);
+	cout << "  ВЫХОД  ";
+	// Просто бесконечный цикл
+	while (z != 111) {
+
+		// Стрелочки выбора
+		if (v == 1) {
+
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+			gotoxy(44, 15);
+			cout << "  ПРОДОЛЖИТЬ  ";
+			gotoxy(46, 17);
+			cout << "  ВЫХОД  ";
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+			gotoxy(46, 13);
+			cout << "  СТАРТ  ";
+		}
+		if (v == 3) {
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+			gotoxy(46, 13);
+			cout << "  СТАРТ  ";
+			gotoxy(44, 15);
+			cout << "  ПРОДОЛЖИТЬ  ";
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+			gotoxy(46, 17);
+			cout << "  ВЫХОД  ";
+		}
+		if (v == 2) {
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+			gotoxy(46, 13);
+			cout << "  СТАРТ  ";
+			gotoxy(46, 17);
+			cout << "  ВЫХОД  ";
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+			gotoxy(44, 15);
+			cout << "  ПРОДОЛЖИТЬ  ";
+		}
+
+		// Это обработчик нажатий
+		if (_kbhit()) {
+			switch (_getch()) {
+			case 'w': {
+				if (v == 1)
+					v = 3;
+				else
+					if (v == 2)
+						v = 1;
+					else
+						if (v == 3)
+							v = 2;
+				break;
+			}
+			case 's': {
+				if (v == 1)
+					v = 2;
+				else
+					if (v == 2)
+						v = 3;
+					else
+						if (v == 3)
+							v = 1;
+				break;
+			}
+			case 'W': {
+				if (v == 1)
+					v = 3;
+				else
+					if (v == 2)
+						v = 1;
+					else
+						if (v == 3)
+							v = 2;
+				break;
+			}
+			case 'S': {
+				if (v == 1)
+					v = 2;
+				else
+					if (v == 2)
+						v = 3;
+					else
+						if (v == 3)
+							v = 1;
+				break;
+			}
+			case 13: {
+				if (v == 1)
+					Start();
+				else
+					if (v == 2)
+						Load();
+					else
+						if (v == 3)
+							Quit();
+				break;
+			}
+			}
+		}
+
+	}
+}
---
+++ SDGame/main.cpp
@@ -0,0 +1,7 @@
+#include "main.h"
+int main() {
+	podgotovka();
+	Menu();
+	
+	return 0;
+}
